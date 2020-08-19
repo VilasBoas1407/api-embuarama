a6b22bf8be24d1788dcae5363432d35f5c9207f7
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-08-19 19:49:17 -0300

    Adicionando GitHub Controller

---
+++ api-embuarama/Controllers/GitHub/apiGitHubController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace api_embuarama.Controllers.GitHub
+{
+    public class apiGitHubController : ApiController
+    {
+        [HttpGet]
+        [Route ("v1/api/github/")]
+        public string Test()
+        {
+            return "Oi";
+        }
+
+    }
+}
