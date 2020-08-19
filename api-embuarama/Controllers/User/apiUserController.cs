fdde0c9ff95500027243fbee588e7974480605f4
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-08-19 20:14:35 -0300

    Adicionando entity

---
+++ api-embuarama/Controllers/User/apiUserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace api_embuarama.Controllers.User
+{
+    public class apiUserController : ApiController
+    {
+        [HttpGet]
+        [Route("v1/api/github/")]
+        public HttpResponseMessage Register()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, new { valid = false, message = "E-mail ou senha incorretos!" });
+        }
+    }
+}
