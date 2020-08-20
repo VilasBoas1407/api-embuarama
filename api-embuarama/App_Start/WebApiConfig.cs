f0dcc251af5df00e341921c8976dd558d2319356
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-08-19 21:38:32 -0300

    Configurando cors

--- api-embuarama/App_Start/WebApiConfig.cs
+++ api-embuarama/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json;
 
 namespace api_embuarama
 {
@@ -10,6 +11,7 @@ namespace api_embuarama
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+
 
             // Web API routes
             config.MapHttpAttributeRoutes();
