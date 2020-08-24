41bea9ba4706ff96531ff28e0baa4eaf96ea20f6
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-08-24 13:58:44 -0300

    Adicionando DTO CompanyRegister.

--- api-embuarama/Controllers/Company/apiCompanyController.cs
+++ api-embuarama/Controllers/Company/apiCompanyController.cs
@@ -1,4 +1,6 @@
-﻿using api_embuarama.Models.Company;
+﻿using api_embuarama.DTO;
+using api_embuarama.Models.Company;
+using api_embuarama.Models.User;
 using api_embuarama.Utils;
 using api_model;
 using System;
@@ -12,27 +14,59 @@ namespace api_embuarama.Controllers.Company
     {
         [HttpPost]
         [Route("v1/api/company/register")]
-        public HttpResponseMessage Register(TB_EMPRESA company)
+        public HttpResponseMessage Register(CompanyRegisterDTO company)
         {
 
             Empresa e = new Empresa();
+            Usuario u = new Usuario();
             Services s = new Services();
 
+            TB_USUARIO Usuario = new TB_USUARIO();
+            TB_EMPRESA Empresa = new TB_EMPRESA();
+
+
             bool emailValido = false;
+            bool emailValidoLogin = false;
             bool cnpjValido = false;
+            bool loginValido = false;
 
             try
             {
                 if (ModelState.IsValid)
                 {
-                    emailValido = e.FindCompanyByEmail(company.DS_EMAIL_EMPRESA);
-                    cnpjValido = e.FindCompanyByCnpj(company.NR_CNPJ);
+                    //Preenchendo dados para criar a empresa
 
-                    if (emailValido && cnpjValido)
+                    Empresa.DS_NOME_RESPONSAVEL = company.DS_NOME_RESPONSAVEL;
+                    Empresa.DS_NOME_EMPRESA = company.DS_NOME_EMPRESA;
+                    Empresa.DS_EMAIL_EMPRESA = company.DS_EMAIL_EMPRESA;
+                    Empresa.NR_CNPJ = company.NR_CNPJ;
+
+                    //Preenchendo dados para criar usuário da empresa
+                    Usuario.FL_EMPRESA = company.FL_EMPRESA;
+                    Usuario.FL_ADMINISTRADOR = false;
+                    Usuario.DS_SENHA = s.CriptografarSenha(company.DS_SENHA);
+                    Usuario.DS_NOME = company.DS_NOME_EMPRESA;
+                    Usuario.DS_LOGIN = company.DS_LOGIN;
+                    Usuario.DS_FUNCAO = "Administrador";
+                    Usuario.DS_EMAIL = company.DS_EMAIL_EMPRESA;
+                    Usuario.NR_DDD = 31;
+                    Usuario.NR_TELEFONE = 998702381;
+                   
+                    emailValido = e.FindCompanyByEmail(company.DS_EMAIL_EMPRESA);
+                    emailValidoLogin = u.FindUserByLogin(Usuario.DS_EMAIL);
+                    cnpjValido = e.FindCompanyByCnpj(company.NR_CNPJ);
+                    loginValido = u.FindUserByLogin(Usuario.DS_LOGIN);
+
+                    if (emailValido && cnpjValido && emailValidoLogin && loginValido)
                     {
-                        company.DS_TOKEN_EMPRESA = s.GerarTokenEmpresa();
-                        company = e.Create(company);
-                        return Request.CreateResponse(HttpStatusCode.OK, new { valid = true, empresa = company });
+                        Empresa.DS_TOKEN_EMPRESA = s.GerarTokenEmpresa();
+                        Empresa = e.Create(Empresa);
+
+                        Usuario.DS_TOKEN_EMPRESA = Empresa.DS_TOKEN_EMPRESA;
+                        Usuario.ID_EMPRESA = Empresa.ID_EMPRESA;
+                        u.Create(Usuario);
+
+                        return Request.CreateResponse(HttpStatusCode.OK, new { valid = true, empresa = Empresa });
                     }
                     else
                     {
@@ -42,6 +76,10 @@ namespace api_embuarama.Controllers.Company
                             message = "Já existe uma empresa com esse e-mail cadastrado!";
                         if (!cnpjValido)
                             message += "Já existe uma empresa com esse CNPJ cadastrado!";
+                        if (!emailValidoLogin)
+                            message += "Já existe um usuário com esse e-mail cadastrado!";
+                        if (!loginValido)
+                            message += "Já existe uma usuário com esse login cadastrado!";
 
                         return Request.CreateResponse(HttpStatusCode.OK, new { valid = false, message=message });
                     }
---
+++ api-embuarama/DTO/CompanyRegisterDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace api_embuarama.DTO
+{
+    public class CompanyRegisterDTO
+    {
+        public int ID_EMPRESA { get; set; }
+        public string DS_NOME_EMPRESA { get; set; }
+        public string NR_CNPJ { get; set; }
+        public string DS_NOME_RESPONSAVEL { get; set; }
+        public string DS_EMAIL_EMPRESA { get; set; }
+        public string DS_TOKEN_EMPRESA { get; set; }
+        public string DS_LOGIN { get; set; }
+        public string DS_SENHA { get; set; }
+        public bool FL_EMPRESA { get; set;}
+    }
+}
